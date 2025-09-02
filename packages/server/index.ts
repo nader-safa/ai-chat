cd147b7fd410fceecae90a4c6b0ec61a5ee2eb51
nader-safa <nader@orthoplexsolutions.com>
2025-09-02 15:59:18 +0300

    initialize and test chat endpoint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import type { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import OpenAI from 'openai'
 
 dotenv.config()
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
 const app = express()
+
+app.use(express.json())
 
 const port = process.env.PORT || 3000
 
@@ -14,6 +21,19 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/v1/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!!' })
+})
+
+app.post('/api/v1/chat', async (req: Request, res: Response) => {
+  const { prompt } = req.body
+
+  const response = await openai.responses.create({
+    model: 'gpt-4o-mini',
+    input: prompt,
+    temperature: 0.2,
+    max_output_tokens: 50,
+  })
+
+  res.json({ message: response.output_text })
 })
 
 app.listen(port, () => {
