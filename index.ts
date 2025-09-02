2d84a05d45a4b481e3fdd35a7473b81584109b95
nader-safa <nader@orthoplexsolutions.com>
2025-09-02 14:51:01 +0300

    run both apps togeather

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently'
+
+concurrently([
+  {
+    command: 'bun run dev',
+    name: 'server',
+    cwd: 'packages/server',
+    prefixColor: 'cyan',
+  },
+  {
+    command: 'bun run dev',
+    name: 'client',
+    cwd: 'packages/client',
+    prefixColor: 'green',
+  },
+])
