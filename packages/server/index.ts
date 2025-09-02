a5c93a754c5a64848df8e74ea3cd7bb5429039a2
nader-safa <nader@orthoplexsolutions.com>
2025-09-02 16:08:08 +0300

    add chat history

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -23,16 +23,20 @@ app.get('/api/v1/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!!' })
 })
 
+const conversations = new Map<string, string>()
+
 app.post('/api/v1/chat', async (req: Request, res: Response) => {
-  const { prompt } = req.body
+  const { prompt, conversationId } = req.body
 
   const response = await openai.responses.create({
     model: 'gpt-4o-mini',
     input: prompt,
     temperature: 0.2,
     max_output_tokens: 50,
+    previous_response_id: conversations.get(conversationId),
   })
 
+  conversations.set(conversationId, response.id)
   res.json({ message: response.output_text })
 })
 
