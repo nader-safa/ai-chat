192d28113c21c77cb8dd8fd69ada697d54cc90f8
nadersafadev <nader@orthoplexsolutions.com>
2025-09-06 03:34:17 +0300

    chore: refactor App component to display health status message from API and clean up commented code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,18 @@
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Button } from './components/ui/button'
 
 function App() {
-  // const [message, setMessage] = useState('THIS IS A TEST')
+  const [message, setMessage] = useState('')
 
-  // useEffect(() => {
-  //   fetch('/api/v1/health')
-  //     .then(response => response.json())
-  //     .then(data => setMessage(data.message))
-  // }, [])
+  useEffect(() => {
+    fetch('/api/v1/health')
+      .then(response => response.json())
+      .then(data => setMessage(data.status))
+  }, [])
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <p>WELCOME TO AI CHAT</p>
+      <p>{message}</p>
 
       <Button>Click me</Button>
     </div>
