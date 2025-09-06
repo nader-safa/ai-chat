47cd46d11ab323c54aa1c0c53627e1c56b5fa773
nadersafadev <nader@orthoplexsolutions.com>
2025-09-06 03:18:51 +0300

    chore: update Vite server configuration to restrict external access to allowed hosts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // allow external access
+    allowedHosts: ['aichat.dishyy.com'], // allow external access
   },
   plugins: [react(), tailwindcss()],
   resolve: {
